f8fcd4e3afbaf26a8153c9a0a3e420ee472f9886
WilliamRMoore <williamrmoore93@gmail.com>
2021-05-06 21:49:43 -0400

    End of section 3

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -38,6 +38,11 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
 
         }
 
@@ -54,6 +59,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
